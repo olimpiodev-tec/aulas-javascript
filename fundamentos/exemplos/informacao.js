4c11ec495d8966c610f5528e6b16d35f40d16f8a
Moisés <olimpiodev.tec@gmail.com>
2026-02-06 09:22:04 -0300

    arquivo informacao

---
+++ fundamentos/exemplos/informacao.js
@@ -0,0 +1,13 @@
+const curso = "Técnico em Desenvolvimento de Sistemas"
+const ano = 2025
+const notaMinimaParaSerAprovado = 50
+const notaParaSerReprovado = 49.9
+const cursoTecnico = true
+
+console.log('Minhas informações acadêmicas')
+console.log("Curso: " + curso)
+console.log(`Ano: ${ano}`)
+console.log(`Nota para aprovação ${notaMinimaParaSerAprovado}`)
+console.log(`Nota mínima para ser reprovado ${notaParaSerReprovado}`)
+console.log(`É curso técnico: ${cursoTecnico}`)
+
