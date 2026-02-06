7c7dd802ee17da6a6ba3709acfc270c91c78e50a
Moisés <olimpiodev.tec@gmail.com>
2026-02-06 09:50:45 -0300

    arquivo saida

---
+++ fundamentos/exemplos/saida.js
@@ -0,0 +1,8 @@
+const nome = prompt("Qual seu primeiro nome?")
+const endereco = prompt("Qual seu endereço?")
+const idade = prompt("Qual sua idade?")
+
+alert(`
+    ${nome}, você tem ${idade} anos,
+    mora no endereço ${endereco}
+    `)
